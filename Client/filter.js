e37efefdd0c7afc0ee82d9355874842c6fd3237c
Lê Văn Tuấn <lt958418@gmail.com>
2026-05-23 01:35:58 +0700

    tan

--- Client/filter.js
+++ Client/filter.js
@@ -1,7 +1,21 @@
 let url = "http://localhost:8080/api/room/getall";
 
 let tanjson = document.querySelector('.call_person');
+document.addEventListener("DOMContentLoaded", () => {
 
+    let username = localStorage.getItem("username");
+    let loginBox = document.getElementById("login-box");
+
+    console.log(username);
+    console.log(loginBox);
+
+    if (username && loginBox) {
+        loginBox.style.display = "none";
+
+        console.log(loginBox.style.display);
+    }
+
+});
 async function callpe() {
 
     try {
@@ -50,19 +64,19 @@ async function callpe() {
 }
 
 callpe();
-let diachi = document.querySelector(".addressFilter");
-let gia_toithieu = document.querySelector(".minPri");
-let gia_toida = document.querySelector(".maxPri");
-let dientich_toithieu = document.querySelector(".minDientich");
-let dientich_toida = document.querySelector(".maxDientich");
-let locketqua = document.querySelector(".locketqua");
-let url = ""
-async function filter() {
+// let diachi = document.querySelector(".addressFilter");
+// let gia_toithieu = document.querySelector(".minPri");
+// let gia_toida = document.querySelector(".maxPri");
+// let dientich_toithieu = document.querySelector(".minDientich");
+// let dientich_toida = document.querySelector(".maxDientich");
+// let locketqua = document.querySelector(".locketqua");
+// let url = ""
+// async function filter() {
 
-    try {
-        let response = await fetch(url, { method: "POST" })
-        let data = await response.json();
+//     try {
+//         let response = await fetch(url, { method: "POST" })
+//         let data = await response.json();
 
-    }
-    catch (error) { console.log(error) };
-}
+//     }
+//     catch (error) { console.log(error) };
+// }
